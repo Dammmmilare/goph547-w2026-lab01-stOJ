6867b77d0b8e9ece0c36f937f3a3adf7995e06ef
Dammmmilare <josh.oyed@gmail.com>
2026-01-31 11:55:59 -0700

    tests and examples updated

---
+++ examples/driver_single_mass.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from src.gravity.py import gravity_potential_mass, gravity_effect_mass
+
+
+# Single Mass Anomaly & Contour Plot
+
+def main():
+
+    # Define parameters for single mass anomaly
+    
---
+++ tests/tests_gravity_functions.py
@@ -0,0 +1,3 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from src.gravity.py import gravity_potential_mass, gravity_effect_mass
